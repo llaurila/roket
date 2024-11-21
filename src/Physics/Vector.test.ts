e2cc1627e336b2ddebf7b05b497e98abea1446a0
Vincent Laurila <lasse@laurila.net>
2024-11-22 01:25:56 +0200

    Removed linter and testing framework

--- src/Physics/Vector.test.ts
+++
@@ -1,9 +0,0 @@
-import Vector from "./Vector";
-
-describe("Vector()", () => {
-    it("initializes the fields", () => {
-        const v = new Vector(1, 2);
-        expect(v.x).toEqual(1);
-        expect(v.y).toEqual(2);
-    });
-});
