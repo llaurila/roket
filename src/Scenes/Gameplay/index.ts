293b34d69271810abeebe101891c1661d95fdf7a
Vincent Laurila <lasse@laurila.net>
2025-07-09 20:23:29 +0300

    Fix YAML quoting for FuelRush

--- src/Scenes/Gameplay/index.ts
+++ src/Scenes/Gameplay/index.ts
@@ -9,6 +9,7 @@ import DeepSpaceMission from "@/Levels/DeepSpaceMission";
 import type Level from "@/Level";
 import type { Viewport } from "@/Graphics/Viewport";
 import GameOfTag from "@/Levels/GameOfTag";
+import FuelRush from "@/Levels/FuelRush";
 import HotLap from "@/Levels/HotLap";
 
 const restartButton = () => Keys.wasPressed("Escape");
@@ -23,6 +24,7 @@ const levelTypes = [
     CollectFuel,
     DeepSpaceMission,
     GameOfTag,
+    FuelRush,
     HotLap
 ];
 
