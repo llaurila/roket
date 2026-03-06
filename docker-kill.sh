8c3c21d8ad64af5cecc46b10da46b7875d138902
Vincent Laurila <lasse.laurila@gmail.com>
2026-03-06 23:46:09 +0200

    removed docker stuff

--- docker-kill.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-docker kill $(docker ps -q --filter ancestor=roket/browser)
--- docker-run.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-
-MOUNT_HOST=$SCRIPT_DIR
-MOUNT_CONTAINER=/app
-
-docker run -p5173:5173 -p5174:5174 -v $MOUNT_HOST:/$MOUNT_CONTAINER -v $MOUNT_CONTAINER/node_modules roket/browser
